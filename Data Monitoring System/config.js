6036e46d21bf7c743539e6590e953dde9c3e571a
kussmavs27 <hentaiheaven27@gmail.com>
2026-05-19 10:50:10 +0800

    for deployment

---
+++ Data Monitoring System/config.js
@@ -0,0 +1,7 @@
+  // config.js
+  export const SUPABASE_CONFIG = {
+    url: "https://ddjurrmsqoznoctxgvab.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkanVycm1zcW96bm9jdHhndmFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NTM2MTksImV4cCI6MjA5MTAyOTYxOX0.fy49gShutb_jiNlw7qHF8Hr22VhWCAxxxQjo0UCv7IQ",
+    serviceRoleKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkanVycm1zcW96bm9jdHhndmFiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTQ1MzYxOSwiZXhwIjoyMDkxMDI5NjE5fQ.KIZoR0tOeq_1QvQ2mJEmjAixo8pdezJby6L9YlBs7Zg"  // Add this line
+
+  };
